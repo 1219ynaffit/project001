9438d5f81d328eb062c2e8a8bb04fa044f4867d8
Tiffany <tiffany@wishingsoft.com>
2023-10-19 21:37:15 +0800

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+﻿set -e
+
+npm run build
+
+cd dist
+
+git init;
+git add -A
+git commit -m 'deploy'
+git push -f https://github.com/1219ynaffit/project001.git main:gh-pages
+
+read -s -n1 -p "success! press any key to continue"
